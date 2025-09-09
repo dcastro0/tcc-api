a2267fba005b4f2e81a0577c5f53e04366939938
Caio <caioccastro1@gmail.com>
2025-09-08 22:19:57 -0300

    Adiciona suporte a CORS na aplicação e inclui Flask-CORS nas dependências

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ from .extensions import db
 from .routes import api
 from config import Config
 import os
+from flask_cors import CORS 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    
+    CORS(app) 
 
     db.init_app(app)
 
